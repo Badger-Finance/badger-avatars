a1d6f56fc857179b66deb7b31b19fa5abd7ef088
Ayush Shukla <shuklaayush247@gmail.com>
2022-08-31 14:52:28 +0400

    chore: add _ to arguments

--- src/aura/AuraAvatarOracleUtils.sol
+++ src/aura/AuraAvatarOracleUtils.sol
@@ -12,21 +12,21 @@ abstract contract AuraAvatarOracleUtils {
     error StalePriceFeed(uint256 currentTime, uint256 updateTime, uint256 maxPeriod);
 
     // TODO: More checks?
-    function fetchPriceFromClFeed(IAggregatorV3 _feed, uint256 maxStalePeriod)
+    function fetchPriceFromClFeed(IAggregatorV3 _feed, uint256 _maxStalePeriod)
         internal
         view
         returns (uint256 answerUint256_)
     {
         (, int256 answer,, uint256 updateTime,) = _feed.latestRoundData();
 
-        if (block.timestamp - updateTime > maxStalePeriod) {
-            revert StalePriceFeed(block.timestamp, updateTime, maxStalePeriod);
+        if (block.timestamp - updateTime > _maxStalePeriod) {
+            revert StalePriceFeed(block.timestamp, updateTime, _maxStalePeriod);
         }
 
         answerUint256_ = uint256(answer);
     }
 
-    function fetchPriceFromBalancerTwap(IPriceOracle _pool, uint256 twapPeriod)
+    function fetchPriceFromBalancerTwap(IPriceOracle _pool, uint256 _twapPeriod)
         internal
         view
         returns (uint256 price_)
@@ -34,14 +34,14 @@ abstract contract AuraAvatarOracleUtils {
         IPriceOracle.OracleAverageQuery[] memory queries = new IPriceOracle.OracleAverageQuery[](1);
 
         queries[0].variable = IPriceOracle.Variable.PAIR_PRICE;
-        queries[0].secs = twapPeriod;
+        queries[0].secs = _twapPeriod;
         queries[0].ago = 0; // now
 
         // Gets the balancer time weighted average price denominated in BAL
         price_ = _pool.getTimeWeightedAverage(queries)[0];
     }
 
-    function fetchBptPriceFromBalancerTwap(IPriceOracle _pool, uint256 twapPeriod)
+    function fetchBptPriceFromBalancerTwap(IPriceOracle _pool, uint256 _twapPeriod)
         internal
         view
         returns (uint256 price_)
@@ -49,7 +49,7 @@ abstract contract AuraAvatarOracleUtils {
         IPriceOracle.OracleAverageQuery[] memory queries = new IPriceOracle.OracleAverageQuery[](1);
 
         queries[0].variable = IPriceOracle.Variable.BPT_PRICE;
-        queries[0].secs = twapPeriod;
+        queries[0].secs = _twapPeriod;
         queries[0].ago = 0; // now
 
         // Gets the balancer time weighted average price denominated in BAL
