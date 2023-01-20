087907ecaf964516b2f08998666f0e44298c6cc3
Petrovska <petrovska@badger.com>
2023-01-20 20:14:36 +0800

    test: cover revert of `doCall` method and expected emitted events

--- test/AvatarTemplate.t.sol
+++ test/AvatarTemplate.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import {console2 as console} from "forge-std/console2.sol";
 
 import {BaseFixture} from "./BaseFixture.sol";
+import {BaseAvatar} from "src/lib/BaseAvatar.sol";
 
 import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
@@ -30,6 +31,25 @@ contract AvatarTemplateTest is BaseFixture {
         assertTrue(success);
         assertEq(token.balanceOf(address(avatar_template)), 0);
         assertEq(token.balanceOf(address(this)), 1e18);
+    }
+
+    function test_doCall_revert() public {
+        ERC20 token = new ERC20("mock", "MCK");
+        vm.label(address(token), "MCK");
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                BaseAvatar.ExecutionFailure.selector,
+                address(token),
+                0,
+                abi.encodeCall(ERC20.transfer, (address(this), 1e18)),
+                block.timestamp
+            )
+        );
+        vm.prank(owner);
+        bool success = avatar_template.doCall(address(token), 0, abi.encodeCall(ERC20.transfer, (address(this), 1e18)));
+
+        assertFalse(success);
     }
 
     function test_doCall_permissions() public {
