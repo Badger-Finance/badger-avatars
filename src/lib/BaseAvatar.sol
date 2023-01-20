c5d8fa3b7d196a1200bdc4ee4953ddefa593182a
Petrovska <petrovska@badger.com>
2023-01-20 19:55:12 +0800

    fix: resolve lack of check on `success` if tx failed when leveraging `doCall` from owner

--- src/lib/BaseAvatar.sol
+++ src/lib/BaseAvatar.sol
@@ -6,9 +6,15 @@ import {Enum, Executor} from "../../lib/safe-contracts/contracts/base/Executor.s
 
 import {GlobalAccessControlManaged} from "./GlobalAccessControlManaged.sol";
 
-/// Avatar
-/// Forwards calls from the owner
+/// @title BaseAvatar
+/// @notice Forwards calls from the owner
 contract BaseAvatar is OwnableUpgradeable, Executor {
+    ////////////////////////////////////////////////////////////////////////////
+    // ERRORS
+    ////////////////////////////////////////////////////////////////////////////
+
+    error ExecutionFailure(address target, uint256 value, bytes data, uint256 timestamp);
+
     ////////////////////////////////////////////////////////////////////////////
     // INITIALIZATION
     ////////////////////////////////////////////////////////////////////////////
@@ -23,7 +29,7 @@ contract BaseAvatar is OwnableUpgradeable, Executor {
     // PUBLIC: Owner
     ////////////////////////////////////////////////////////////////////////////
 
-    /// @dev Make arbitrary Ethereum call
+    /// @dev Make arbitrary Ethereum call. Can only be called by owner.
     /// @param to Address to call
     /// @param value ETH value
     /// @param data TX data
@@ -35,5 +41,6 @@ contract BaseAvatar is OwnableUpgradeable, Executor {
         returns (bool success)
     {
         success = execute(to, value, data, Enum.Operation.Call, gasleft());
+        if (!success) revert ExecutionFailure(to, value, data, block.timestamp);
     }
 }
